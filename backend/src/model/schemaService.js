689c8b2ffb62767f41e8e74cc9ee6e8e661ace5a
EvoDev405 <ozielkambou602@gmail.com>
2025-11-28 15:08:41 +0000

    dossier

--- backend/src/model/schemaService.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose")
-const schemaService = new mongoose.Schema({
-    libelle:{type:String,required:true}
-},{timestamps:true})
-
-const Service = mongoose.model("service",schemaService)
-module.exports = Service
